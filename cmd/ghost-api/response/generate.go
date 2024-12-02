e803beb3456666d6b14ac9d81a17c9162b54f8df
ghost2023 <nathnaelwonisha@gmail.com>
2024-12-02 10:30:52 +0300

    fix: issue with spacing

--- cmd/ghost-api/response/generate.go
+++ cmd/ghost-api/response/generate.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"time"
 
@@ -102,9 +103,9 @@ func GenerateString(node *config.Node) (string, error) {
 				return "", e
 			}
 
-			finalString += word.(string)
+			finalString += " " + word.(string)
 		}
-		return finalString, nil
+		return strings.TrimSpace(finalString), nil
 	} else {
 		s := getRandString(node.Metadata)
 		return s, nil
